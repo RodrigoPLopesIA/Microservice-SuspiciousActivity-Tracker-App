be32b4ca639564897c5ee4d6243fe7fe68baf3c4
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-30 12:47:11 -0300

    feat: create project

---
+++ src/main/java/com/rodrigo/ms/suspicious_activity_tracker/SuspiciousActivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.ms.suspicious_activity_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuspiciousActivityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuspiciousActivityTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/ms/suspicious_activity_tracker/SuspiciousActivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.ms.suspicious_activity_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuspiciousActivityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
