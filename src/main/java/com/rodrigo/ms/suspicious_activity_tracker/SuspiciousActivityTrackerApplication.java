4364763b10ff3fa609498db3daa574a12997b8e7
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-31 00:37:00 -0300

    feat: add lombok and enable jpa auditing

--- src/main/java/com/rodrigo/ms/suspicious_activity_tracker/SuspiciousActivityTrackerApplication.java
+++ src/main/java/com/rodrigo/ms/suspicious_activity_tracker/SuspiciousActivityTrackerApplication.java
@@ -2,8 +2,10 @@ package com.rodrigo.ms.suspicious_activity_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SuspiciousActivityTrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rodrigo/ms/suspicious_activity_tracker/entities/SuspiciousActivity.java
+++ src/main/java/com/rodrigo/ms/suspicious_activity_tracker/entities/SuspiciousActivity.java
@@ -1,15 +1,59 @@
 package com.rodrigo.ms.suspicious_activity_tracker.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "suspicious_activities")
+@EntityListeners(AuditingEntityListener.class)
 public class SuspiciousActivity {
 
-
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(generator = "UUID")
     private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "endpoint", length = 255)
+    private String endpoint;
+
+    @Column(name = "ip_address", length = 45)
+    private String ipAddress;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false, name = "created_at")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false, name = "updated_at")
+    private Instant updatedAt;
+
+   
 }
