ffcc55da52f0adebe5004224f9aa517afa646994
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-09-01 12:09:31 -0300

    feat: add DTOs for Request and Response of Suspicious Activity

---
+++ src/main/java/com/rodrigo/ms/suspicious_activity_tracker/dto/RequestSuspiciousActivityDTO.java
@@ -0,0 +1,18 @@
+package com.rodrigo.ms.suspicious_activity_tracker.dto;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RequestSuspiciousActivityDTO(
+        @NotBlank UUID userId,
+        @NotBlank @Pattern(
+            regexp = "^(https?:\\/\\/)?([a-zA-Z0-9\\-]+\\.)+[a-zA-Z]{2,}(:\\d+)?(\\/.*)?$", 
+            message = "Invalid Endpoint"
+            ) String endpoint,
+        @NotBlank @Size(max = 45) @Pattern(regexp = "^[0-9.]+$", message = "Invalid IP Address") String ipAddress,
+        @NotBlank String description) {
+
+}
