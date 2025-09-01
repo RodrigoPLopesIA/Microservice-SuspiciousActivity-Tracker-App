17cc524c991a5d4b992f088dc9a9de8c7f5a78a8
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-09-01 11:57:16 -0300

    feat: implement SuspiciousActivityService for managing suspicious activity data

---
+++ src/main/java/com/rodrigo/ms/suspicious_activity_tracker/services/SuspiciousActivityService.java
@@ -0,0 +1,18 @@
+package com.rodrigo.ms.suspicious_activity_tracker.services;
+
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.ms.suspicious_activity_tracker.repositories.SuspiciousActivityRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SuspiciousActivityService {
+
+    private final SuspiciousActivityRepository suspiciousActivityRepository;
+
+    public void save(SuspiciousActivityDTO data) {
+        suspiciousActivityRepository.save(data);
+    }
+}
