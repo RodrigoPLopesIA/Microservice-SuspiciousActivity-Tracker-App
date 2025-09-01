b79d409c3288f0435bcbb61cce982afd32972e39
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-09-01 12:21:15 -0300

    feat: enhance SuspiciousActivityService to save and return ResponseSuspiciousActivityDTO

--- src/main/java/com/rodrigo/ms/suspicious_activity_tracker/services/SuspiciousActivityService.java
+++ src/main/java/com/rodrigo/ms/suspicious_activity_tracker/services/SuspiciousActivityService.java
@@ -3,6 +3,9 @@ package com.rodrigo.ms.suspicious_activity_tracker.services;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.ms.suspicious_activity_tracker.dto.RequestSuspiciousActivityDTO;
+import com.rodrigo.ms.suspicious_activity_tracker.dto.ResponseSuspiciousActivityDTO;
+import com.rodrigo.ms.suspicious_activity_tracker.entities.SuspiciousActivity;
+import com.rodrigo.ms.suspicious_activity_tracker.mapper.SuspiciousActivityMapper;
 import com.rodrigo.ms.suspicious_activity_tracker.repositories.SuspiciousActivityRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +16,19 @@ public class SuspiciousActivityService {
 
     private final SuspiciousActivityRepository suspiciousActivityRepository;
 
-    public void save(RequestSuspiciousActivityDTO data) {
-        suspiciousActivityRepository.save(data);
+
+    private final ProducerService producerService;
+
+    private final SuspiciousActivityMapper dataMapper;
+
+    public ResponseSuspiciousActivityDTO save(RequestSuspiciousActivityDTO data) {
+
+        var dataToSave = dataMapper.toEntity(data);
+
+        var savedData = suspiciousActivityRepository.save(dataToSave);
+
+        producerService.sendMessage(savedData);
+        
+        return dataMapper.toResponseDTO(savedData);
     }
 }
